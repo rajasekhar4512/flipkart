b2effb213b2eba7b3561d4b44a62ca32b14ecce4
rajasekhar4512 <rajasekhar4512@gmail.com>
2018-06-26 09:08:55 -0700

    have written helloworld program

--- login.java
+++ login.java
@@ -1,7 +1,8 @@
-public class HelloWorld
-public static void main string args
-system.out.println
-
+public class HelloWorld{
+ public static void main(String args[]){
+  System.out.println("HelloWorld");
+}
+}
 
 
 
